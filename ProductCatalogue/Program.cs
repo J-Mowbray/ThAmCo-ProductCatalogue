f627b05763430519d5e93eda27b4dde56ae2387a
J-Mowbray <j.mowbray96@gmail.com>
2025-03-21 22:37:21 +0000

    Added Polly and updated Environment settings

--- ProductCatalogue/Program.cs
+++ ProductCatalogue/Program.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using ProductCatalogue.Data.Products;
 using ProductCatalogue.Services.UnderCutters;
 using System.Security.Claims;
- 
+using Polly;
+using Polly.Extensions.Http;
+using ProductCatalogue.Services.ProductsRepo;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,6 +29,40 @@ if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddSingleton<IUnderCuttersService, UnderCuttersServiceFake>();
 }
+builder.Services.AddDbContext<ProductsContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        var dbPath = System.IO.Path.Join(path, "products.db");
+        options.UseSqlite($"Data Source={dbPath}");
+        options.EnableDetailedErrors();
+        options.EnableSensitiveDataLogging();
+    }
+    else
+    {
+        var cs = builder.Configuration.GetConnectionString("ProductsContext");
+        options.UseSqlServer(cs, sqlServerOptionsAction: sqlOptions =>
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(3),
+                errorNumbersToAdd: null
+            )
+        );
+    }
+});
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSingleton<IUnderCuttersService, UnderCuttersServiceFake>();
+    
+}
+else
+{
+   
+}
+builder.Services.AddTransient<IProductsRepo, ProductsRepo>();
 
 var app = builder.Build();
 
