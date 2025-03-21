6b7a72f5ce11853ca549e55c9c3eb61bf70e4094
J-Mowbray <j.mowbray96@gmail.com>
2025-03-21 22:36:26 +0000

    Updated debug controller to use our product repo

--- ProductCatalogue/Controllers/DebugController.cs
+++ ProductCatalogue/Controllers/DebugController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ProductCatalogue.Services.ProductsRepo;
 using ProductCatalogue.Services.UnderCutters;
 
 namespace ProductCatalogue.Controllers;
@@ -10,12 +11,15 @@ public class DebugController : ControllerBase
 {
     private readonly ILogger _logger;
     private readonly IUnderCuttersService _underCuttersService;
+    private readonly IProductsRepo _productsRepo;
     
     public DebugController(ILogger<DebugController> logger,
-                                        IUnderCuttersService underCuttersService)
+                                        IUnderCuttersService underCuttersService,
+                                        IProductsRepo productsRepo)
     {
         _logger = logger;
         _underCuttersService = underCuttersService;
+        _productsRepo = productsRepo;
     }
 
     // GET: /debug/undercutters
@@ -33,6 +37,25 @@ public class DebugController : ControllerBase
         {
             _logger.LogWarning("Exception occured when using the UnderCutters service");
             products = Array.Empty<ProductDto>();
+        }
+        return Ok(products.ToList());
+    }
+
+        // GET: /debug/repo
+    [HttpGet("repo")]
+    public async Task<IActionResult> Repo()
+    
+    {
+        IEnumerable<Product> products = null!;
+        try
+        {
+            products = await _productsRepo.GetProductsAsync();
+  
+        }
+        catch
+        {
+            _logger.LogWarning("Exception occured when using the Products repo");
+            products = Array.Empty<Product>();
         }
         return Ok(products.ToList());
     }
